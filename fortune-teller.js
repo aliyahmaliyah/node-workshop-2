ee3564278f72bbd2d804d2ecb0a2c2635d07ccc9
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-02-02 22:09:12 +0000

    repeating fortune using process argv

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,3 +1,12 @@
-var fortune = require('./library/fortune.js')
+var fortune = require('./library/fortune.js');
 
-console.log(fortune());
+process.argv.forEach(function(val, index, array){
+ if (index === 2)
+ for (var i = 0; i < Number(val); i++){
+ console.log(fortune());
+}});
+
+process.argv.forEach(function(val,index,array){
+  if (index === 2)
+  console.log(val);
+})
